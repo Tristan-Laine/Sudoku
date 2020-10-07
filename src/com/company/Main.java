0de4234869029e432eb9bfb478df775d0778a8a9
Tristan Laine <tristan.laine@edu.itescia.fr>
2020-10-07 14:25:17 +0200

    story 2 experimentale

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -17,11 +17,29 @@ public class Main {
         }
     }
 
+    public static boolean ContraintesLigne(int [][] Tableau){
+        boolean Nombre = true;
+
+        for (int i=0; i < 9; i++) {
+            for (int nb = 0; nb < 9; nb++) {
+                if ((Tableau[0][i] == Tableau[0][nb]) && (Tableau[0][i] != 0) && (i != nb)) {
+                    int FAUX = Tableau[0][i];
+                    Nombre = false;
+                    System.out.print(FAUX + " est déjà sur la ligne. ");
+                }
+            }
+        }
+        return Nombre;
+    }
+
+
+
+
     public static void main(String[] args) {
 	// write your code here
 
                 int[][] boardEasy = {
-                {4, 0, 0, 1, 0, 2, 6, 8, 0},
+                {4, 0, 0, 1, 1, 2, 6, 8, 0},
                 {1, 0, 0, 0, 9, 0, 0, 0, 4},
                 {0, 3, 8, 0, 6, 4, 0, 1, 0},
                 {0, 0, 5, 0, 7, 1, 9, 2, 0},
@@ -33,6 +51,7 @@ public class Main {
                 };
 
     TableauAffichage(boardEasy);
+    ContraintesLigne(boardEasy);
 
 
     }
