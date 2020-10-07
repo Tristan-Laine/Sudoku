ee75683c0e6bba9a234fba5fb053a99f6c9337fe
Tristan Laine <tristan.laine@edu.itescia.fr>
2020-10-07 15:07:46 +0200

    story 2 et 3 fini

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -21,7 +21,7 @@ public class Main {
         boolean Nombre = true;
 
         for (int i=0; i < 9; i++) {
-            for (int nb = 0; nb < 9; nb++) {
+            for (int nb = i+1; nb < 9; nb++) {
                 if ((Tableau[0][i] == Tableau[0][nb]) && (Tableau[0][i] != 0) && (i != nb)) {
                     int FAUX = Tableau[0][i];
                     Nombre = false;
@@ -29,9 +29,27 @@ public class Main {
                 }
             }
         }
+        System.out.println();
+        System.out.println(Nombre);
         return Nombre;
     }
 
+    public static boolean ContraintesColonne(int [][] Tableau){
+        boolean Nombre = true;
+
+        for (int i=0; i < 9; i++) {
+            for (int nb = i+1; nb < 9; nb++) {
+                if ((Tableau[i][0] == Tableau[nb][0]) && (Tableau[i][0] != 0) && (i != nb)) {
+                    int FAUX = Tableau[i][0];
+                    Nombre = false;
+                    System.out.print(FAUX + " est déjà sur la colonne. ");
+                }
+            }
+        }
+        System.out.println();
+        System.out.println(Nombre);
+        return Nombre;
+    }
 
 
 
@@ -39,11 +57,11 @@ public class Main {
 	// write your code here
 
                 int[][] boardEasy = {
-                {4, 0, 0, 1, 1, 2, 6, 8, 0},
+                {4, 0, 0, 1, 2, 2, 6, 8, 0},
                 {1, 0, 0, 0, 9, 0, 0, 0, 4},
                 {0, 3, 8, 0, 6, 4, 0, 1, 0},
                 {0, 0, 5, 0, 7, 1, 9, 2, 0},
-                {0, 2, 6, 0, 0, 9, 8, 0, 0},
+                {8, 2, 6, 0, 0, 9, 8, 0, 0},
                 {8, 0, 0, 2, 5, 0, 0, 0, 0},
                 {9, 0, 3, 0, 0, 0, 0, 0, 8},
                 {2, 5, 0, 6, 0, 0, 1, 0, 7},
@@ -52,7 +70,7 @@ public class Main {
 
     TableauAffichage(boardEasy);
     ContraintesLigne(boardEasy);
-
+    ContraintesColonne(boardEasy);
 
     }
 }
